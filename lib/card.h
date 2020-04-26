f15dc07cecdaf421644cb01ebc16306ebe007a3d
koenigni <koenigni@student.ethz.ch>
2020-04-27 00:07:47 +0200

    asdf

--- lib/card.h
+++ lib/card.h
@@ -29,39 +29,23 @@ typedef unsigned int card_collection;
 
 typedef char card_id;
 
-int
-card_get_id(card *, card_id *);
-int
-card_get(card_id *, card *);
-int
-card_get_name(card_id *, char *);
-int
-card_get_score(card_id *, int *);
+int card_get_id(card *, card_id *);
+int card_get(card_id *, card *);
+int card_get_name(card_id *, char *);
+int card_get_score(card_id *, int *);
 
-int
-card_collection_contains(card_collection *, card_id *, int *);
-int
-card_collection_add_card(card_collection *, card_id *);
-int
-card_collection_add_card_array(card_collection *, card_id *, int);
-int
-card_collection_remove_card(card_collection *, card_id *);
-int
-card_collection_get_card_count(card_collection *, int *);
-int
-card_collection_get_card(card_collection *, int, card_id *);
-int
-card_collection_get_score(card_collection *, int *);
-int
-card_collection_empty(card_collection *);
-int
-card_collection_fill(card_collection *);
-int
-card_collection_draw_random(card_collection *, card_id *);
+int card_collection_contains(card_collection *, card_id *, int *);
+int card_collection_add_card(card_collection *, card_id *);
+int card_collection_add_card_array(card_collection *, card_id *, int);
+int card_collection_remove_card(card_collection *, card_id *);
+int card_collection_get_card_count(card_collection *, int *);
+int card_collection_get_card(card_collection *, int, card_id *);
+int card_collection_get_score(card_collection *, int *);
+int card_collection_empty(card_collection *);
+int card_collection_fill(card_collection *);
+int card_collection_draw_random(card_collection *, card_id *);
 
 typedef struct game_rules game_rules;
 
-int
-stich_card_legal(card_id *, card_id, card_collection *, int, game_rules *);
-int
-stich_get_winner(game_rules *, card_id *, int *);
+int stich_card_legal(card_id *, card_id, card_collection *, int, game_rules *);
+int stich_get_winner(game_rules *, card_id *, int *);
--- lib/server.h
+++ lib/server.h
@@ -15,31 +15,19 @@ typedef struct server {
   pthread_t conn_listener;
 } server;
 
-int
-server_has_player_id(server *, player_id);
-connection_s2c *
-server_get_free_connection(server *);
-connection_s2c *
-server_get_connection_by_pid(server *, player_id);
-player *
-server_get_player_by_pid(server *, player_id);
-void
-server_add_player(server *, player *);
-void
-server_notify_join(server *, player *);
-void
-server_resync_player(server *, player *);
+int server_has_player_id(server *, player_id);
+connection_s2c *server_get_free_connection(server *);
+connection_s2c *server_get_connection_by_pid(server *, player_id);
+player *server_get_player_by_pid(server *, player_id);
+void server_add_player(server *, player *);
+void server_notify_join(server *, player *);
+void server_resync_player(server *, player *);
 
-void
-server_acquire_state_lock(server *);
-void
-server_release_state_lock(server *);
+void server_acquire_state_lock(server *);
+void server_release_state_lock(server *);
 
-void
-server_disconnect_connection(server *, connection_s2c *);
+void server_disconnect_connection(server *, connection_s2c *);
 
-void
-server_tick(server *s, long time);
+void server_tick(server *s, long time);
 
-void
-server_distribute_event(server *, event *, void (*)(event *, player *));
+void server_distribute_event(server *, event *, void (*)(event *, player *));
