79daaae04ffe017880ac53ff1d1eee0aff74384a
Dichloromethane <45919918+Dichloromethane@users.noreply.github.com>
2020-04-28 02:48:34 +0200

    Only open /dev/urandom once

--- src/util.c
+++ src/util.c
@@ -4,17 +4,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int
-util_rand_int(const int min, const int max) {
-  int fd = open("/dev/urandom", O_RDONLY);
-  if (fd == -1) {
+int random_fd;
+
+__attribute__((constructor(101))) void 
+open_random_fd (void) {
+  random_fd = open("/dev/urandom", O_RDONLY);
+  if (random_fd == -1) {
 	perror("Error while accessing '/dev/urandom': ");
 	exit(1);
   }
+}
 
+int
+util_rand_int(const int min, const int max) {
   int random;
-  read(fd, &random, sizeof(int));
-  close(fd);
+  read(random_fd, &random, sizeof(int));
 
   return (random % (max - min)) + min;
 }
