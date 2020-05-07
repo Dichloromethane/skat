6d66a8d3d936c325993afa945ee7be0223d482fd
iTitus <iTitus@users.noreply.github.com>
2020-05-07 20:05:26 +0200

    fix for cmake

--- conf/conf.h
+++ conf/conf.h
@@ -1,4 +1,4 @@
 #pragma once
 
 #define DEFAULT_PORT (6666)
-#define DEBUG_PRINTF 
+#define DEBUG_PRINTF
--- include/skat/util.h
+++ include/skat/util.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include"conf.h"
+#include "conf.h"
 
 int util_rand_int(int min, int max);
 
 #ifdef DEBUG_PRINTF
-#define DEBUG_NOTIFY(fmt, ...) DEBUG_PRINTF_RAW("NOTIFY: " fmt)
+#define DEBUG_NOTIFY(fmt, ...)     DEBUG_PRINTF_RAW("NOTIFY: " fmt)
 #define DEBUG_NOTIFY_RAW(fmt, ...) dprintf(2, fmt, __VA_ARGS__)
 #endif
