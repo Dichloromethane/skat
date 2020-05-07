3d15498dfe5cbfc46a280a3bbecbeb8ca41d5da5
Dichloromethane <45919918+Dichloromethane@users.noreply.github.com>
2020-05-07 23:58:29 +0200

    Fixed timer

--- conf/conf.h
+++ conf/conf.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define DEFAULT_PORT (6666)
-#define SERVER_REFRESH_RATE (1)
+#define SERVER_REFRESH_RATE (2)
 
 #define HAS_DEBUG_PRINTF 
 
--- src/skat/ctimer.c
+++ src/skat/ctimer.c
@@ -16,8 +16,11 @@ void
 ctimer_create(ctimer *t, void *arg, void (*timerf)(void *), int nsecs) {
   struct sigevent sev;
 
+  DEBUG_PRINTF("Creating timer");
+  
   t->timerf = timerf;
   t->arg = arg;
+  t->nsecs = nsecs;
   sem_init(&t->activations, 0, 0);
 
   sev.sigev_notify = SIGEV_THREAD;
@@ -25,20 +28,20 @@ ctimer_create(ctimer *t, void *arg, void (*timerf)(void *), int nsecs) {
   sev.sigev_notify_function = ctimer_tick;
   sev.sigev_notify_attributes = NULL;
 
-  timer_create(CLOCK_REALTIME, &sev, &t->timer_id);
+  ERRNO_CHECK(timer_create(CLOCK_REALTIME, &sev, &t->timer_id));
 }
 
 _Noreturn void
 ctimer_run(ctimer *t) {
-  struct itimerspec dummy;
   struct itimerspec itspec;
 
+  DEBUG_PRINTF("Setting timer to %d", t->nsecs);
   itspec.it_interval.tv_nsec = t->nsecs;
   itspec.it_interval.tv_sec = 0;
   itspec.it_value.tv_nsec = 0;
-  itspec.it_value.tv_sec = 0;
+  itspec.it_value.tv_sec = 1;
 
-  ERRNO_CHECK(timer_settime(t->timer_id, 0, &itspec, &dummy));
+  ERRNO_CHECK(timer_settime(t->timer_id, 0, &itspec, NULL));
 
   DEBUG_PRINTF("Started Server Timer");
   for (;;) {
