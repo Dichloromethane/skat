dd9aac82485b93cd480d6539309e2350eb9de35e
iTitus <iTitus@users.noreply.github.com>
2020-09-12 00:57:19 +0200

    add a few null checks

--- src/skat/utf8.c
+++ src/skat/utf8.c
@@ -7,10 +7,12 @@ utf8_valid(const char *utf8_str) {
   const unsigned char *str = (const unsigned char *) utf8_str;
 
   // better be safe than sorry ;)
-  if ((str[0] & 0b11111000u) == 0b11110000u
-	  && (str[1] & 0b11000000u) == 0b10000000u
-	  && (str[2] & 0b11000000u) == 0b10000000u
-	  && (str[3] & 0b11000000u) == 0b10000000u) {// 4 bytes
+  if (!str) {
+	return 0;
+  } else if ((str[0] & 0b11111000u) == 0b11110000u
+			 && (str[1] & 0b11000000u) == 0b10000000u
+			 && (str[2] & 0b11000000u) == 0b10000000u
+			 && (str[3] & 0b11000000u) == 0b10000000u) {// 4 bytes
 	return 1;
   } else if ((str[0] & 0b11110000u) == 0b11100000u
 			 && (str[1] & 0b11000000u) == 0b10000000u
@@ -31,10 +33,13 @@ utf8_codepoint(const char *utf8_str, unicode_codepoint_t *out_cp) {
   const unsigned char *str = (const unsigned char *) utf8_str;
 
   // better be safe than sorry ;)
-  if ((str[0] & 0b11111000u) == 0b11110000u
-	  && (str[1] & 0b11000000u) == 0b10000000u
-	  && (str[2] & 0b11000000u) == 0b10000000u
-	  && (str[3] & 0b11000000u) == 0b10000000u) {// 4 bytes
+  if (!str) {
+	DERROR_PRINTF("null pointer");
+	exit(1);
+  } else if ((str[0] & 0b11111000u) == 0b11110000u
+			 && (str[1] & 0b11000000u) == 0b10000000u
+			 && (str[2] & 0b11000000u) == 0b10000000u
+			 && (str[3] & 0b11000000u) == 0b10000000u) {// 4 bytes
 	if (out_cp)
 	  *out_cp = ((str[0] & 0b00000111u) << 18u)
 				| ((str[1] & 0b00111111u) << 12u)
@@ -66,8 +71,11 @@ utf8_codepoint(const char *utf8_str, unicode_codepoint_t *out_cp) {
 
 void
 utf8_convert_unicode_codepoint(unicode_codepoint_t cp, char *out_utf8_cp) {
-  if ((cp & 0xffe00000u) != 0) {// >21 bits, invalid
-	DERROR_PRINTF("Given GLFW codepoint invalid for utf8");
+  if (!out_utf8_cp) {
+	DERROR_PRINTF("null pointer");
+	exit(1);
+  } else if ((cp & 0xffe00000u) != 0) {// >21 bits, invalid
+	DERROR_PRINTF("Given codepoint is invalid");
 	exit(1);
   } else if ((cp & 0x001f0000u) != 0) {// >16 bits, 4 bytes
 	out_utf8_cp[0] = (char) (0b11110000u | ((cp >> 18u) & 0b00000111u));
@@ -91,6 +99,11 @@ utf8_convert_unicode_codepoint(unicode_codepoint_t cp, char *out_utf8_cp) {
 
 size_t
 utf8_length(const char *utf8_str) {
+  if (!utf8_str) {
+	DERROR_PRINTF("null pointer");
+	exit(1);
+  }
+
   size_t length = 0;
 
   for (const char *next = utf8_str; *next; length++)
