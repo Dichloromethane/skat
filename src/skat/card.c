3a4ee4531d297650ccb1f0e5d782cf8da96ae70b
iTitus <iTitus@users.noreply.github.com>
2020-10-27 21:47:38 +0100

    simplify sort logic

--- src/skat/card.c
+++ src/skat/card.c
@@ -3,7 +3,7 @@
 
 const char *const CARD_TYPE_NAMES = "789DK0AB";
 const uint8_t CARD_SCORES[] = {0, 0, 0, 3, 4, 10, 11, 2};
-const uint8_t CARD_COMPARE_TYPE_NULL[] = {0, 1, 2, 5, 6, 3, 7, 4};
+const uint8_t COMPARE_CARD_TYPE_NULL[] = {0, 1, 2, 5, 6, 3, 7, 4};
 const char *const CARD_COLOR_NAMES = "KaHePiKr";
 
 int
@@ -58,7 +58,7 @@ card_get_score(const card_id *const cid, uint8_t *const score) {
 static int
 card_compare_id(const card_id *const cid1, const card_id *const cid2,
 				const game_rules *const gr) {
-  return *cid1 - *cid2;
+  return (int) *cid1 - (int) *cid2;
 }
 
 static int
@@ -69,36 +69,38 @@ card_compare_hand(const card_id *const cid1, const card_id *const cid2,
 	  || card_get(cid2, &c2))
 	return 0;
 
-  uint8_t adj_id1 = 0, adj_id2 = 0;
-
+  // color order
   if (gr->type == GAME_TYPE_NULL || gr->type == GAME_TYPE_RAMSCH) {
-	adj_id1 |= ((c1.cc - 1) & 0b111u) << 3u;
-	adj_id2 |= ((c2.cc - 1) & 0b111u) << 3u;
-  } else if (gr->type == GAME_TYPE_GRAND) {
-	adj_id1 |= ((c1.ct == CARD_TYPE_B ? 4 : (c1.cc - 1)) & 0b111u) << 3u;
-	adj_id2 |= ((c2.ct == CARD_TYPE_B ? 4 : (c2.cc - 1)) & 0b111u) << 3u;
-  } else {// GAME_TYPE_COLOR
-	uint8_t adj_cc1 = c1.ct == CARD_TYPE_B  ? 5
-					  : c1.cc == gr->trumpf ? 4
-											: (c1.cc - 1);
-	adj_id1 |= (adj_cc1 & 0b111u) << 3u;
+	if (c1.cc != c2.cc)
+	  return (int) c1.cc - (int) c2.cc;
+  } else {// GAME_TYPE_GRAND/GAME_TYPE_COLOR
+	uint8_t adj_cc1;
+	if (c1.ct == CARD_TYPE_B)
+	  adj_cc1 = 6;
+	else if (gr->type == GAME_TYPE_COLOR && c1.cc == gr->trumpf)
+	  adj_cc1 = 5;
+	else
+	  adj_cc1 = c1.cc;
 
-	uint8_t adj_cc2 = c2.ct == CARD_TYPE_B  ? 5
-					  : c2.cc == gr->trumpf ? 4
-											: (c2.cc - 1);
-	adj_id2 |= (adj_cc2 & 0b111u) << 3u;
+	uint8_t adj_cc2;
+	if (c2.ct == CARD_TYPE_B)
+	  adj_cc2 = 6;
+	else if (gr->type == GAME_TYPE_COLOR && c2.cc == gr->trumpf)
+	  adj_cc2 = 5;
+	else
+	  adj_cc2 = c2.cc;
+
+	if (adj_cc1 != adj_cc2)
+	  return (int) adj_cc1 - (int) adj_cc2;
   }
 
+  // type order
   if (gr->type == GAME_TYPE_COLOR || gr->type == GAME_TYPE_GRAND
-	  || gr->type == GAME_TYPE_RAMSCH) {
-	adj_id1 |= (c1.ct - 1) & 0b111u;
-	adj_id2 |= (c2.ct - 1) & 0b111u;
-  } else {// GAME_TYPE_NULL
-	adj_id1 |= CARD_COMPARE_TYPE_NULL[c1.ct - 1] & 0b111u;
-	adj_id2 |= CARD_COMPARE_TYPE_NULL[c2.ct - 1] & 0b111u;
-  }
-
-  return (int16_t) adj_id1 - (int16_t) adj_id2;
+	  || gr->type == GAME_TYPE_RAMSCH)
+	return (int) c1.ct - (int) c2.ct;
+  else// GAME_TYPE_NULL
+	return (int) COMPARE_CARD_TYPE_NULL[c1.ct - 1]
+		   - (int) COMPARE_CARD_TYPE_NULL[c2.ct - 1];
 }
 
 static int
@@ -114,7 +116,7 @@ card_compare_stiche(const card_id *const cid1, const card_id *const cid2,
 	return 0;
 
   if (score1 != score2)
-	return (int16_t) score1 - (int16_t) score2;
+	return (int) score1 - (int) score2;
 
   return card_compare_hand(cid1, cid2, gr);
 }
